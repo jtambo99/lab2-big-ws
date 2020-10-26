62af6701fa1b1dbb7d1a0803d48e54ba0af93f1b
jtambo99 <jtambo@live.com>
2020-10-26 10:10:13 +0100

    Added JUnit "Expected" clause to te tests

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import translator.Application;
 import translator.domain.TranslatedText;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 
@@ -16,13 +18,13 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest(classes = Application.class)
 public class TranslatorServiceTest {
 
-  @Autowired
-  TranslatorService translatorService;
+    @Autowired
+    TranslatorService translatorService;
 
-  @Test
-  public void translateTest() {
-    TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-    assertEquals("I don't know how to translate from en to es the text 'This is a test of translation service'", translatedText.getTranslation());
-  }
+    @Test(expected = RuntimeException.class)
+    public void translateTest() {
+        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
+        assertEquals("I don't know how to translate from en to es the text 'This is a test of translation service'", translatedText.getTranslation());
+    }
 
 }
--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ClassUtils;
+import org.springframework.ws.client.WebServiceIOException;
+import org.springframework.ws.client.WebServiceTransportException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 import translator.Application;
@@ -25,28 +27,56 @@ import static org.junit.Assert.assertNotNull;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class TranslatorEndpointTest {
 
-  private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    private final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 
-  @LocalServerPort
-  private int port;
+    @LocalServerPort
+    private int port;
 
-  @Before
-  public void init() throws Exception {
-    marshaller.setPackagesToScan(ClassUtils.getPackageName(GetTranslationRequest.class));
-    marshaller.afterPropertiesSet();
-  }
+    @Before
+    public void init() throws Exception {
+        marshaller.setPackagesToScan(ClassUtils.getPackageName(GetTranslationRequest.class));
+        marshaller.afterPropertiesSet();
+    }
 
-  @Test
-  public void testSendAndReceive() {
-    GetTranslationRequest request = new GetTranslationRequest();
-    request.setLangFrom("en");
-    request.setLangTo("es");
-    request.setText("This is a test of translation service");
-    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
-            + port + "/ws", request);
-    assertNotNull(response);
-    assertThat(response, instanceOf(GetTranslationResponse.class));
-    GetTranslationResponse translation = (GetTranslationResponse) response;
-    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
-  }
+    @Test(expected = RuntimeException.class)
+    public void testSendAndReceive() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+                + port + "/ws", request);
+        assertNotNull(response);
+        assertThat(response, instanceOf(GetTranslationResponse.class));
+        GetTranslationResponse translation = (GetTranslationResponse) response;
+        assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+    }
+
+    @Test(expected = WebServiceIOException.class)
+    public void testUnknownHost() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhostUnknown:"
+                + port + "/ws", request);
+        assertNotNull(response);
+        assertThat(response, instanceOf(GetTranslationResponse.class));
+        GetTranslationResponse translation = (GetTranslationResponse) response;
+        assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+    }
+
+    @Test(expected = WebServiceTransportException.class)
+    public void testServiceFailure() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+                + port + "/wsFallo", request);
+        assertNotNull(response);
+        assertThat(response, instanceOf(GetTranslationResponse.class));
+        GetTranslationResponse translation = (GetTranslationResponse) response;
+        assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+    }
 }
